e6167d7ea088b02f043c313598f257c85580aa6d
Josh Lopez <lopezja4@uci.edu>
2018-11-07 21:27:23 -0800

    added devices page

--- server.js
+++ server.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 const router = express.Router();
+app.use(express.static('static'))
 
+// TODO: Delete later
+var mockData = [];
+var products = ["VP5300", "Augusta S", "VP8800", "VP6300"]
+var status = ["Connected", "RKI In Progess", "Offline"]
+for (var i = 0; i < 100; ++i) {
+  mockData[i] = {
+    "deviceID" : "US-WC-CA-" + (300 - i),
+    "product" : products[i % products.length],
+    "modelNo" : "model" + i,
+    "serialNo" : "s" + i,
+    "lastStatus" : status[i % products.length],
+    "lastHeartbeat" : new Date(Date.now() - Math.random()).toString()
+  }
+}
+// End TODO: Delete Later
+
+// Middleware function checks if user is logged in before accessing a page.
 function requiresLogin(req, res, next) {
   if (req.session && req.session.userId) {
     return next();
@@ -11,6 +29,21 @@ function requiresLogin(req, res, next) {
     err.status = 401;
     return next(err);
   }
+}
+
+// Utility function retrieves most recent N entries
+function mostRecent(data, N=5) {
+  const dateLabel = "lastHeartbeat";
+  return data.sort(function(entryX, entryY) {
+    const x = entryX[dateLabel], y = entryY[dateLabel];
+    if (x < y) {
+      return -1;
+    }
+    if (x > y) {
+      return 1;
+    }
+    return 0;
+  }).slice(N * -1);
 }
 
 // Login/Home Page
@@ -35,7 +68,11 @@ router.post('/home', function(req, res) {
 
 // Device/Event Page
 router.get('/devices', function(req, res) {
-	res.render('pages/device')
+  var fetched = mostRecent(mockData);
+  console.log(fetched)
+  res.render('pages/device', {
+        data: fetched
+    });
 });
 
 // Mount the router on the app
