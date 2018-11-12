6b8a5c24f061176126a6b2e075c7305ac3536d74
Josh Lopez <lopezja4@uci.edu>
2018-11-12 10:26:09 -0800

    added gitignore, made sidebar icon pretty

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
-const router = express.Router();
-app.use(express.static('static'));
+var express = require('express');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 
-// TODO: Delete later
+var app = express();
+var router = express.Router();
+
+// Set static files folder
+app.use(express.static('static'));
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Mount the router on the app
+var urlEncodedParser = bodyParser.urlencoded({extended: true});
+app.use(urlEncodedParser)
+app.use(bodyParser.json())
+app.use(session({secret: "secret key"}));
+app.use('/', router);
+
+app.listen(3000, function () {
+    console.log('Running on port 3000')
+});
+
+// TODO: Replace Mock Data with actual data
 var mockData = [];
 var products = ["VP5300", "Augusta S", "VP8800", "VP6300"]
 var status = ["Connected", "RKI In Progess", "Offline"]
@@ -21,7 +38,7 @@ for (var i = 0; i < 100; ++i) {
     "lastHeartbeat" : new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toString()
   }
 }
-// End TODO: Delete Later
+// End TODO
 
 // Middleware function checks if user is logged in before accessing a page.
 function requiresLogin(req, res, next) {
@@ -34,46 +51,73 @@ function requiresLogin(req, res, next) {
 }
 
 // Utility function retrieves most recent N entries
-function mostRecent(data, N=30) {
+function mostRecent(data, reverse=false, N=30) {
   const dateLabel = "lastHeartbeat";
   return data.sort(function(x, y) {
     const a = new Date(x[dateLabel]), b = new Date(y[dateLabel]);
+    if (reverse)
+      return b - a;
     return a - b;
   }).slice(N * -1);
 }
 
-// Login/Home Page
+function sortBy(data, sortingFunction, N=30) {
+  return data.sort(sortingFunction).slice(N * -1);
+}
+
+// Home Page
 router.get('/', function (req, res) {
 	// User is logged in
   if (req.session && req.session.user) {
 	    res.render('pages/home');
 	}
-	// User not logged in
-    res.render('pages/login');
+  else {
+    // User not logged in
+    res.redirect('/login');
+  }
 });
 
+// Two Factor Authentication
 router.get('/twofact', function (req, res) {
   res.render('pages/twofact');
-})
-
-// Submit Login Form (post request to '/')
-router.post('/', function(req, res) {
-  req.session.user = {id: "user", password: "pw"};
-	res.redirect('/twofact');
 });
-
-
-// Submit Verification Form (post request to '/')
 router.post('/twofact', function(req, res) {
   res.redirect('/');
+});
+
+// Login page
+router.get('/login', function(req, res) {
+  return res.render('pages/login');
+});
+router.post('/login', function(req, res) {
+  req.session.user = {id: req.body.username, password: req.body.password};
+	return res.redirect('/twofact');
 });
 
 // Device/Event Page
 router.get('/devices', function(req, res) {
   var fetched = mostRecent(mockData);
   res.render('pages/devices', {
-        data: fetched
-    });
+    sortby: "mostrecent",
+    data: fetched
+  });
+});
+
+router.post('/devices', urlEncodedParser, (req, res) => {
+  var sortingMethod = req.body.sortby;
+  var fetched = [];
+  switch(sortingMethod) {
+    case "mostrecent":
+      fetched = mostRecent(mockData);
+      break;
+    case "leastrecent":
+      fetched = mostRecent(mockData, reverse=true);
+      break;
+  }
+  res.render('pages/devices', {
+    sortby: sortingMethod,
+    data: fetched
+  });
 });
 
 // Profile Page
@@ -89,12 +133,4 @@ router.get('/logout', function(req, res) {
       res.redirect('/');
     });
   }
-});
-
-// Mount the router on the app
-app.use(session({secret: "secret key"}));
-app.use('/', router);
-
-app.listen(3000, function () {
-    console.log('Running on port 3000')
 });
--- static/js/universal.js
+++ static/js/universal.js
@@ -18,36 +18,32 @@ function focusField(elem) {
 function displaySidebar() {
   if ($(".sidebar").length > 0)
     return false;
-
-  var buttons = [$("#profile-btn"), $("#devices-btn"), $("#logout-btn")];
-  var contents = "<ul class='side sidenav-list'>";
-  for (var button of buttons)
-    if (button.length > 0)
-      contents += "<li class='side sidenav-holder'>" + button.prop('outerHTML') + "</li>";
-  contents += "</ul>";
-  $("body").append("<div class='side sidebar'>" + contents + "</div>");
-  $(".sidebar").find("button").attr("class","gtbtn side");
-  $("body").append("<div class='side navcolor'></div>");
-  $(".navcolor").css({left: (-document.documentElement.clientWidth) + "px", background: $(".sidebar").css("background"), display: "block", width: document.documentElement.clientWidth + "px"});
-  $(".sidebar-mobile-icon-holder").find('path').css('fill','#ffffff');
-  $(".navcolor").animate({left: "0px"}, 300);
-  $(".sidebar").animate({left: "0px"}, 300, function(){
-    $(".sidebar-mobile-icon-holder").unbind('click');
-    $(".sidebar-mobile-icon-holder").click(hideSidebar);
-  });
+  else {
+    document.getElementById("sidebar-icon").src = "/img/sidebar/icon-close.gif"
+    var buttons = [$("#profile-btn"), $("#devices-btn"), $("#logout-btn")];
+    var contents = "<ul class='side sidenav-list'>";
+    for (var button of buttons)
+      if (button.length > 0)
+        contents += "<li class='side sidenav-holder'>" + button.prop('outerHTML') + "</li>";
+    contents += "</ul>";
+    $("body").append("<div class='side sidebar'>" + contents + "</div>");
+    $(".sidebar").find("button").attr("class","gtbtn side");
+    $(".sidebar").animate({left: "0px"}, 300, function(){
+      $(".sidebar-mobile-icon-holder").unbind('click');
+      $(".sidebar-mobile-icon-holder").click(hideSidebar);
+    });
+  }  
 }
 
 function hideSidebar() {
   if ($(".sidebar").length == 0)
     return false;
-  $(".sidebar-mobile-icon-holder").find('path').css('fill','#ffffff');
-  $(".navcolor").animate({left: (-document.documentElement.clientWidth) + "px"}, 300, function() {
-    $(".navcolor").remove();
-  });
-  $(".sidebar").animate({left: "-325px"}, 300, function(){
-    $(".sidebar").remove();
-    $(".sidebar-mobile-icon-holder").unbind('click');
-    $(".sidebar-mobile-icon-holder").click(displaySidebar);
-  });
-  
+  else {
+    document.getElementById("sidebar-icon").src = "/img/sidebar/icon-open.gif"
+    $(".sidebar").animate({left: "-325px"}, 300, function(){
+      $(".sidebar").remove();
+      $(".sidebar-mobile-icon-holder").unbind('click');
+      $(".sidebar-mobile-icon-holder").click(displaySidebar);
+    });
+  }
 }
